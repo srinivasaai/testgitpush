7787193311ed5f1f5b839d6b2a0be96a78533111
srinivasaai <srinivasaai@gmail.com>
2022-07-23 17:26:12 +0530

    this is my first code push in git

--- test_sri.py
+++ test_sri.py
@@ -1,8 +1,49 @@
-asdfkasd
-from ;'sdfg' \
-      'asdfgvsdaf' \
-      ':argasdfgv' \
-      'asdf' \
-      'gasdf' \
-      ':argasdfgasdfg' \
-      'asdfg'
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://srinivasaai:HAREKrsna@cluster0.dgka1.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+#{} called document
+data = {
+      'name':'srinivas',
+      'email_id':'srinivasaai@gmail.com',
+      'surname':'thummedala',
+      'subject': ['Data science','big data','data analytics']
+}
+
+
+list_of_records = [
+       {'companyName': 'iNeuron',
+     'product': 'Affordable AI',
+     'courseOffered': 'Machine Learning with Deployment'},
+
+      {'companyName': 'iNeuron',
+     'product': 'Affordable AI',
+     'courseOffered': 'Deep Learning for NLP and Computer vision'},
+
+    {'companyName': 'iNeuron',
+     'product': 'Master Program',
+     'courseOffered': 'Data Science Masters Program'} ]
+
+
+database = client['myinfo']
+collection = database['sri']
+# collection.insert_one(data)
+collection.insert_many(list_of_records)
+
+collection1 = database["dpkt"]
+data1 = [  {"Time": "09:15",
+            "askPrice": 6722.65,
+            "askQty": 750,
+            "bidQty": 750,
+            "bidprice": 6389.45},
+            {"Time": "09:15",
+            "askPrice": 6711.55,
+            "askQty": 500,
+            "bidQty": 500,
+            "bidprice": 6215.2}]
+
+collection1.insert_one(data1)
+
+
+
